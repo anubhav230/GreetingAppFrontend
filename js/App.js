ae2d2c23403a5f1a2ccfa9a9632a38181a8866db
anubhav singh <anubhavsingh230@gmail.com>
2020-09-14 13:10:50 +0530

    ADDED: Ability to delete greeting from api

--- js/App.js
+++ js/App.js
@@ -1,28 +1,31 @@
 
+// let list = [
+//     {
+//         message: "Hello",
+//         name: "Anubhav Singh"
 
-let list = [
-    {
-        message: "Hello",
-        name: "Anubhav Singh"
+//     },
+//     {
+//         message: "Hello",
+//         name: "Krishna"
 
-    },
-    {
-        message: "Hello",
-        name: "Krishna"
+//     },
+//     {
+//         message: "Hello",
+//         name: "Krishna"
 
-    },
-    {
-        message: "Hello",
-        name: "Krishna"
+//     },
+// ]
 
-    },
-]
+function showList() {
+    location.reload();
+}
 
 let url = "http://localhost:3000"
 
 function getAllMessages() {
     fetch(`${url}`)
-    .then(data => {return data.json()})
+        .then(data => { return data.json() })
         .then(res => {
             console.log(res.message);
             displayData(res.message)
@@ -30,7 +33,6 @@ function getAllMessages() {
         .catch(err => console.log(err));
 }
 getAllMessages();
-
 
 
 function displayData(list) {
@@ -43,7 +45,7 @@ function displayData(list) {
         let id = document.createElement("div");
         id.className = "id-div";
         id.innerText = list[i]._id
-        id.innerText = "ObjectId("+list[i]._id+")";
+        id.innerText = "ObjectId(" + list[i]._id + ")";
         // let name = document.createElement("div");
         // name.className = "name-div";
         // name.innerText = list[i].name;
@@ -59,6 +61,64 @@ function displayData(list) {
         listOfContent.appendChild(messageContent);
     };
 }
+
+
+function add() {
+    let firstName = document.getElementById('addFirstName').value;
+    let lastName = document.getElementById('addLastName').value;
+    console.log(firstName);
+    console.log(lastName);
+
+    addMessage(firstName, lastName);
+}
+
+function addMessage(firstname, lastname) {
+    // await fetch(`${url}/${firstname}/${lastname}`,{
+    //     method: 'POST'
+    // });
+    // let greeting = {
+    //     firstname: firstname,
+    //     lastname: lastname,
+    // }
+
+    fetch('http://localhost:3000/', {
+        method: "POST",
+        body: JSON.stringify({
+            firstname: firstname,
+            lastname: lastname,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(data => { return data.json()})
+        .then(res => {
+            console.log(res);
+        }).catch(err => { return err });
+}
+
+
+function deleteGrerting() {
+    let id = document.getElementById('addId').value;
+    deleteMessage(id);
+}
+
+function deleteMessage(id){
+    console.log(id);
+    fetch('http://localhost:3000/'+id,{
+        headers:{
+            "content-type":"application/json; charset=UTF-8"
+        },
+        method: "DELETE"
+    })
+    .then(data => {return data.json()})
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => console.log(err));
+}
+
+
 
 
 
