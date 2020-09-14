277e2d49d038accc7cb3b72e85f8e2fe628ccfa4
anubhav singh <anubhavsingh230@gmail.com>
2020-09-15 05:13:23 +0530

    ADDED: Ability to add new greeting

--- js/App.js
+++ js/App.js
@@ -1,22 +1,3 @@
-
-// let list = [
-//     {
-//         message: "Hello",
-//         name: "Anubhav Singh"
-
-//     },
-//     {
-//         message: "Hello",
-//         name: "Krishna"
-
-//     },
-//     {
-//         message: "Hello",
-//         name: "Krishna"
-
-//     },
-// ]
-
 function showList() {
     location.reload();
 }
@@ -30,33 +11,39 @@ function getAllMessages() {
             console.log(res.message);
             displayData(res.message)
         })
-        .catch(err => console.log(err));
+        .catch(err => console.log(err)
+
+        );
 }
 getAllMessages();
 
-
 function displayData(list) {
+    console.log(list)
     let listOfContent = document.querySelector(".list-of-greeting");
-    // console.log(listOfContent);
-    let box = document.createElement("div");
+    // let box = document.createElement("div");
     for (let i = 0; i < list.length; i++) {
         let messageContent = document.createElement("div");
         messageContent.className = "message-content-box";
         let id = document.createElement("div");
         id.className = "id-div";
-        id.innerText = list[i]._id
         id.innerText = "ObjectId(" + list[i]._id + ")";
-        // let name = document.createElement("div");
-        // name.className = "name-div";
-        // name.innerText = list[i].name;
+        let name = document.createElement("div")
+        name.className = "name-div"
+        name.innerText = list[i].firstName + " " + list[i].lastName;
         let message = document.createElement("div");
         message.className = "message-div";
-        message.innerText = list[i].message;
+        message.innerText = "Hello";
         let time = document.createElement("div");
         time.className = "time-div";
         time.innerText = "15 min ago"
         messageContent.appendChild(id);
-        messageContent.appendChild(message);
+        if (list[i].firstName == undefined && list[i].lastName == undefined) {
+            message.innerText = "Hello World"
+            messageContent.appendChild(message);
+        } else {
+            messageContent.appendChild(message);
+            messageContent.appendChild(name); 
+        }
         messageContent.appendChild(time);
         listOfContent.appendChild(messageContent);
     };
@@ -66,34 +53,29 @@ function displayData(list) {
 function add() {
     let firstName = document.getElementById('addFirstName').value;
     let lastName = document.getElementById('addLastName').value;
-    console.log(firstName);
-    console.log(lastName);
 
     addMessage(firstName, lastName);
 }
 
 function addMessage(firstname, lastname) {
-    // await fetch(`${url}/${firstname}/${lastname}`,{
-    //     method: 'POST'
-    // });
-    // let greeting = {
-    //     firstname: firstname,
-    //     lastname: lastname,
-    // }
-
     fetch('http://localhost:3000/', {
+
+        headers: {
+            'Accept': 'application/json',
+            "Content-type": "application/json; charset=UTF-8"
+        },
         method: "POST",
         body: JSON.stringify({
-            firstname: firstname,
-            lastname: lastname,
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
+            firstName: firstname,
+            lastName: lastname,
+        })
+
     })
-        .then(data => { return data.json()})
+        .then(data => { return data.json() })
         .then(res => {
             console.log(res);
+            alert("Success");
+            location.reload();
         }).catch(err => { return err });
 }
 
@@ -103,22 +85,19 @@ function deleteGrerting() {
     deleteMessage(id);
 }
 
-function deleteMessage(id){
+function deleteMessage(id) {
     console.log(id);
-    fetch('http://localhost:3000/'+id,{
-        headers:{
-            "content-type":"application/json; charset=UTF-8"
+    fetch('http://localhost:3000/' + id, {
+        headers: {
+            "content-type": "application/json; charset=UTF-8"
         },
         method: "DELETE"
     })
-    .then(data => {return data.json()})
-    .then(res => {
-        console.log(res);
-    })
-    .catch(err => console.log(err));
+        .then(data => { return data.json() })
+        .then(res => {
+            console.log(res);
+            alert("Success");
+            location.reload();
+        })
+        .catch(err => console.log(err));
 }
-
-
-
-
-
